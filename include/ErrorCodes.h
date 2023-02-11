9dc7b1b44c48f89f7791971aa8396192dfc6a3a4
David Grechkin <123dg32@gmail.com>
2023-02-11 21:05:36 +0200

    added ErrorCodes.h, to store all errorcodes

---
+++ include/ErrorCodes.h
@@ -0,0 +1,2 @@
+#define BAD_CONFIGURATION_ERR_MSG ("An error has occurred while parsing the config file.")
+#define FAILED_BOOT_ERR_MSG ("An error has occurred during the booting process.")
---
+++ src/bootmenu.c
@@ -0,0 +1,16 @@
+#include "../include/bootmenu.h"
+#include "../include/ErrorCodes.h"
+
+
+boot_menu_cfg_s bmcfg; // boot menu config
+void StartBootManager()
+{
+
+
+    //InitBootMenuConfig()
+    // for later, when reading from config file
+    while(TRUE)
+    {
+
+    }
+}
