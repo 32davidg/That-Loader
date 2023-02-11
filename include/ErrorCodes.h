6ce86541eee7832f88438b4df43c13c5e0fb73b3
David Grechkin <123dg32@gmail.com>
2023-02-11 21:08:52 +0200

    added utils file header, to hold defines for all utilities

--- include/ErrorCodes.h
+++ include/ErrorCodes.h
@@ -1,2 +1,4 @@
+#pragma once
+#include <uefi.h>
 #define BAD_CONFIGURATION_ERR_MSG ("An error has occurred while parsing the config file.")
 #define FAILED_BOOT_ERR_MSG ("An error has occurred during the booting process.")
---
+++ include/bootutils.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <uefi.h>
+// This utility file holds the neccesary utilities for the boot proc
+
+#define FALSE ((boolean_t)0)
+#define TRUE ((boolean_t)1)
