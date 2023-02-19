7171c7cae732afbe3eed38d1d8d90ed125be8761
David Grechkin <123dg32@gmail.com>
2023-02-19 15:48:47 +0200

    added shell commands basics

--- src/commands.c
+++ src/commands.c
@@ -1,1 +1,47 @@
 #include "../include/commands.h"
+#include "../include/shellutils.h"
+#include "../include/shelldefs.h"
+#include "../include/bootutils.h"
+#include "../include/ErrorCodes.h"
+
+// Include all commands that can be executed
+/*
+#include "cmds/pwd.h"
+#include "cmds/ls.h"
+#include "cmds/cd.h"
+#include "cmds/touch.h"
+#include "cmds/mkdir.h"
+#include "cmds/clear.h"
+#include "cmds/cat.h"
+#include "cmds/reboot.h"
+#include "cmds/shutdown.h"
+#include "cmds/rm.h"
+#include "cmds/edit.h"
+#include "cmds/passwd.h"
+#include "cmds/cp.h"
+#include "cmds/about.h"
+*/
+
+
+//List of all commands
+/*
+const shell_cmd_s commands[] = {
+ { "help",     HelpCmd,     HelpBrief,     HelpLong },
+{ "pwd",      PwdCmd,      PwdBrief,      NULL },
+{ "ls",       LsCmd,       LsBrief,       LsLong },
+{ "cd",       CdCmd,       CdBrief,       CdLong },
+{ "touch",    TouchCmd,    TouchBrief,    TouchLong },
+{ "mkdir",    MkdirCmd,    MkdirBrief,    MkdirLong },
+{ "clear",    ClearCmd,    ClearBrief,    NULL },
+{ "cat",      CatCmd,      CatBrief,      CatLong },
+{ "reboot",   RebootCmd,   RebootBrief,   RebootLong },
+{ "shutdown", ShutdownCmd, ShutdownBrief, NULL },
+{ "rm",       RmCmd,       RmBrief,       RmLong },
+{ "edit",     EditCmd,     EditBrief,     EditLong },
+{ "passwd",   PasswdCmd,   PasswdBrief,   PasswdLong },
+{ "cp",       CpCmd,       CpBrief,       CpLong },
+{ "about",    AboutCmd,    AboutBrief,    NULL },
+{ "", NULL, NULL, NULL } // Has to be here in order to terminate the command counter
+};  
+*/
+ 
