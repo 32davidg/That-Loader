22af4f0cae1e1924850b094ea26398dc58e2831c
David Grechkin <123dg32@gmail.com>
2023-02-20 11:13:58 +0200

    .

--- src/bootutils.c
+++ src/bootutils.c
@@ -147,7 +147,7 @@ void EnableWatchdogTimer(uintn_t seconds)
 // The function reads the file content into a dynamically allocated buffer (null terminated)
 // The buffer must be freed by the user
 // outFileSize is an optional parameter, it will contain the file size
-char_t* printGetFileContent(char_t* path, uint64_t* outFileSize)
+char_t* GetFileContent(char_t* path, uint64_t* outFileSize)
 {
     char_t* buffer = NULL;
     FILE* file = fopen(path, "r");
@@ -185,11 +185,7 @@ efi_status_t GetFileInfo(efi_file_handle_t* fileHandle, efi_file_info_t* fileInf
     efi_guid_t infGuid = EFI_FILE_INFO_GUID;
     uintn_t size = sizeof(efi_file_info_t);
     return fileHandle->GetInfo(fileHandle, &infGuid, &size, (void*)fileInfo);
-
-
-
 }
-
 
 // Returns the size of the file in bytes
 uint64_t GetFileSize(FILE* file)
