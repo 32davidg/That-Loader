cca927c9ec7253e3281b575ed11caa8923187e91
David Grechkin <123dg32@gmail.com>
2023-02-20 14:09:10 +0200

    changed graphics

--- include/graphics.h
+++ include/graphics.h
@@ -1,89 +1,22 @@
-#pragma once
-#include <uefi.h>
+#include <uefi.h> 
+#define DEFAULT_CONSOLE_COLUMNS (80)
+#define DEFAULT_CONSOLE_ROWS    (30);
 
-// GRAPHICS
-
-// This sets the color of the pixels ( Graphics Color )
-void SetGraphicsColor(uint32_t color)
-{
-    EFI_GRAPHICS_OUTPUT_BLT_PIXEL GColor;
-    GColor.Reserved = color >> 24;
-    GColor.Red      = color >> 16;
-    GColor.Green    = color >> 8;
-    GColor.Blue     = color;
-    GraphicsColor = GColor;
+void print_border(int MAX_ROWS, int MAX_COLS) {
+    for (int row = 0; row < MAX_ROWS; row++) {
+        for (int col = 0; col < MAX_COLS; col++) {
+            if (row == 0 || row == MAX_ROWS-1 || col == 0 || col == MAX_COLS-1) {
+                ST->ConOut->SetAttribute(ST->ConOut, EFI_TEXT_ATTR(EFI_WHITE, EFI_WHITE)); // higlight text
+                printf("*");
+                ST->ConOut->SetAttribute(ST->ConOut, EFI_TEXT_ATTR(EFI_LIGHTGRAY, EFI_BLACK)); // go back to normal
+            }
+            else {
+                ST->ConOut->SetAttribute(ST->ConOut, EFI_TEXT_ATTR(EFI_WHITE, EFI_WHITE)); // higlight text
+                printf("*");
+                ST->ConOut->SetAttribute(ST->ConOut, EFI_TEXT_ATTR(EFI_LIGHTGRAY, EFI_BLACK)); // go back to normal
+            }
+        }
+        printf("\n");
+    }
 }
-typedef uintn_t              EFI_PHYSICAL_ADDRESS;
 
-// UEFI 2.9 Specs PDF Page 491
-typedef enum EFI_GRAPHICS_OUTPUT_BLT_OPERATION
-{
-    EfiBltVideoFill,
-    EfiBltVideoToBltBuffer,
-    EfiBltBufferToVideo,
-    EfiBltVideoToVideo,
-    EfiGraphicsOutputBltOperationMax
-} EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
-
-// UEFI 2.9 Specs PDF Page 491
-typedef struct EFI_GRAPHICS_OUTPUT_BLT_PIXEL
-{
-    uint8_t   Blue;
-    uint8_t   Green;
-    uint8_t   Red;
-    uint8_t   Reserved;
-} EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
-
-// UEFI 2.9 Specs PDF Page 486
-typedef enum EFI_GRAPHICS_PIXEL_FORMAT
-{
-    PixelRedGreenBlueReserved8BitPerColor,
-    PixelBlueGreenRedReserved8BitPerColor,
-    PixelBitMask,
-    PixelBltOnly,
-    PixelFormatMax
-} EFI_GRAPHICS_PIXEL_FORMAT;
-
-// UEFI 2.9 Specs PDF Page 485
-typedef struct EFI_PIXEL_BITMASK
-{
-    uint32_t    RedMask;
-    uint32_t    GreenMask;
-    uint32_t    BlueMask;
-    uint32_t    ReservedMask;
-} EFI_PIXEL_BITMASK;
-
-// UEFI 2.9 Specs PDF Page 486
-typedef struct EFI_GRAPHICS_OUTPUT_MODE_INFORMATION
-{
-    uint32_t                      Version;
-    uint32_t                      HorizontalResolution;
-    uint32_t                      VerticalResolution;
-    EFI_GRAPHICS_PIXEL_FORMAT   PixelFormat;
-    EFI_PIXEL_BITMASK           PixelInformation;
-    uint32_t                      PixelsPerScanLine;
-} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
-
-// UEFI 2.9 Specs PDF Page 488
-typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE
-{
-    uint32_t                                MaxMode;
-    uint32_t                                Mode;
-    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION  *Info;
-    uintn_t                                 SizeOfInfo;
-    EFI_PHYSICAL_ADDRESS                  FrameBufferBase;
-    uintn_t                                 FrameBufferSize;
-} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
-
-typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, uint32_t ModeNumber, uintn_t *SizeOfInfo, EFI_GRAPHICS_OUTPUT_MODE_INFORMATION **Info);
-typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, uint32_t ModeNumber);
-typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT)(struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer, EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation, uintn_t SourceX, uintn_t SourceY, uintn_t DestinationX, uintn_t DestinationY, uintn_t Width, uintn_t Height, uintn_t Delta);
-
-// UEFI 2.9 Specs PDF Page 485
-typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL
-{
-    EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE  QueryMode;
-    EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE    SetMode;
-    EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT         Blt;
-    EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE        *Mode;
-} EFI_GRAPHICS_OUTPUT_PROTOCOL;
--- src/bootmenu.c
+++ src/bootmenu.c
@@ -40,7 +40,7 @@ static void PrintTimeout(void);
 boot_menu_cfg_s bmcfg; // boot menu config
 void StartBootManager()
 {
-
+    print_border(30, 80);
     InitBootMenuOutput();
     while(TRUE)
     {
